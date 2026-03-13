3dcecebad00599cc027dc0ea6419b5c49d58d39b
Qahramon <qbegboyev@gmail.com>
2026-03-13 11:26:32 +0500

    Commint

--- Event/urls.py
+++ Event/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
-from log.views import LogEventView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('log', LogEventView.as_view()),
+    path('api', include('log.urls')),
 ]
